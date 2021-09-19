64131bd4d95783671c2b7d0b18ccbb8fdc0e0a8a
Mukola1983 <yalok844@gmail.com>
2021-09-19 16:21:55 +0300

    add Api

--- src/API/rovers.js
+++ src/API/rovers.js
@@ -1,12 +1,15 @@
-import Utils from './Utils';
+import Utils from './Utils'
 
-const API_KEY = process.env.REACT_APP_MARS_KEY;
+// const API_KEY = process.env.REACT_APP_MARS_KEY;
 
-// const API_KEY = '59CDafTnK5TeXMtpaRwlrBGoJJ0WoLciNEVfIe5z';
+const API_KEY = '59CDafTnK5TeXMtpaRwlrBGoJJ0WoLciNEVfIe5z'
 
 export const getPhoto = (roverName, page, sol, camera) =>
   Utils.fetchJSON(
     `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=${sol}&page=${page}&${camera}&api_key=${API_KEY}`
-  );
+  )
 
-export const manifest = () => Utils.fetchJSON(`https://api.nasa.gov/mars-photos/api/v1/rovers/?api_key=${API_KEY}`);
+export const manifest = () =>
+  Utils.fetchJSON(
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/?api_key=${API_KEY}`
+  )
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState } from 'react'
+import { getPhoto } from './API/rovers'
 
 export default function App() {
   const [userInput, setUserInput] = useState('dfsdfsd')
@@ -10,11 +11,20 @@ export default function App() {
   const clickHandler = () => {
     setUserInput(value)
   }
+
+  const getApi = () => {
+    getPhoto('Curiosity', 1, 1)
+      .then((data) => {
+        console.log(data.photos)
+      })
+      .catch(() => {})
+  }
   return (
     <div>
       <p>{userInput}</p>
+      <button onClick={getApi}>sssss</button>
       <input type="text" onChange={ochangeInput} />
-      <button onClick={clickHandler}>click</button>
+      <button onClick={clickHandler}>clsssick</button>
     </div>
   )
 }
